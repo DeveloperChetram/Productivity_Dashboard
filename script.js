051ef76f8816ced5af6a041448ff65ad9eb84465
chetram <patelchetram49@gmail.com>
2025-05-13 15:32:43 +0530

    this is the commit of Productivity Dhashboard

---
+++ script.js
@@ -0,0 +1,25 @@
+const openFeatures= (()=>{
+    let cards = document.querySelectorAll('.card');
+let span = document.querySelector('.card span');
+let pages = document.querySelectorAll('.page');
+let cross = document.querySelectorAll('.page i');
+
+cards.forEach((card)=>{
+    card.addEventListener("click",()=>{
+        // console.log(pages[card.id]);
+       pages[card.id].style.display = "block";
+
+    })
+    
+})
+cross.forEach((element, index)=>{
+    element.addEventListener("click",()=>{
+         pages[index].style.display = "none";
+    })
+})
+ 
+   
+
+})()
+
+
